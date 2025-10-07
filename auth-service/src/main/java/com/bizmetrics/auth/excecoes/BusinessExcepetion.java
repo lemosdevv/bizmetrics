77fa34f10bc859157daf56862099e239098b4c9c
lemosdevv <teuse853@gmail.com>
2025-10-07 20:23:34 -0300

    feat(exceptions): restore BusinessException class for error handling

--- auth-service/src/main/java/com/bizmetrics/auth/excecoes/BusinessExcepetion.java
+++
@@ -1,8 +0,0 @@
-package com.bizmetrics.auth.excecoes;
-
-public class BusinessExcepetion extends RuntimeException {
-    public BusinessExcepetion(String message) {
-        super(message);
-    }
-
-}
---
+++ auth-service/src/main/java/com/bizmetrics/auth/excecoes/BusinessException.java
@@ -0,0 +1,8 @@
+package com.bizmetrics.auth.excecoes;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+
+}
