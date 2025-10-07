736ee053c4fe7df20de929043dc869ccc8b659c7
lemosdevv <teuse853@gmail.com>
2025-10-07 20:08:05 -0300

    refactor(dtos): streamline UserDto field annotations for improved readability

--- auth-service/src/main/java/com/bizmetrics/auth/dtos/UserDto.java
+++ auth-service/src/main/java/com/bizmetrics/auth/dtos/UserDto.java
@@ -3,24 +3,16 @@ package com.bizmetrics.auth.dtos;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record UserDto(
-    @NotBlank
-    @Size(max = 50)
-    String username,
+    
+@NotBlank @Size(max = 50) String username,
+    @NotBlank @Size(min = 8, max = 72) String password,
+    @NotBlank @Email @Size(max = 100) String email,
+    @NotNull @Positive Long companyId
 
-    @NotBlank
-    @Size(max = 100)
-    String password,
-
-    @NotBlank
-    @Email
-    @Size(max = 100)
-    String email,
-
-    @NotNull
-    Long companyId
 ) {
 
 }
